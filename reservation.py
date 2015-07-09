7ed1013b1ad2bd853d87cd7f2d53f718930bd961
Dominic Cerquetti <binary1230@gmail.com>
2015-07-09 19:35:53 -0400

    WIp overlapping support, fix unit tests to support this something is still borked, however.

--- reservation.py
+++ reservation.py
@@ -1,4 +1,35 @@
-hour_tolerance = 1.5   # number of hours away two dates can be to be the "same"
+hour_tolerance = 0.45   # number of hours away two dates can be to be the "same"
+
+
+# a date comparison class that
+class FuzzyDate:
+    def __init__(self, _date):
+        self.date = _date
+
+    @staticmethod
+    def _is_there_only_a_small_gap_between(date1, date2):
+        seconds_tolerance = hour_tolerance * 60 * 60
+        diff = date1 - date2
+        num_seconds_diff = abs(diff.total_seconds())
+        return num_seconds_diff  < seconds_tolerance
+
+    def __lt__(self, other):
+        return self.date < other.date or FuzzyDate._is_there_only_a_small_gap_between(self.date, other.date)
+
+    def __le__(self, other):
+        return self.date <= other.date or FuzzyDate._is_there_only_a_small_gap_between(self.date, other.date)
+
+    def __eq__(self, other):
+        return self.date == other.date or FuzzyDate._is_there_only_a_small_gap_between(self.date, other.date)
+
+    def __ne__(self, other):
+        return self.date != other.date and not FuzzyDate._is_there_only_a_small_gap_between(self.date, other.date)
+
+    def __gt__(self, other):
+        return self.date > other.date or FuzzyDate._is_there_only_a_small_gap_between(self.date, other.date)
+
+    def __ge__(self, other):
+        return self.date >= other.date or FuzzyDate._is_there_only_a_small_gap_between(self.date, other.date)
 
 
 class RoomReservation:
@@ -8,20 +39,32 @@ class RoomReservation:
 
     # attempts to extend the room reservation using the given times
     # returns true if we could extend, false if we could not
-    def extend_if_possible(self, reservation):
-        if self._is_there_no_gap_between(self.end_dt, reservation.start_dt):
-            self.end_dt = reservation.end_dt
+    def extend_if_possible(self, reservation):        
+        start_A = FuzzyDate(reservation.start_dt)
+        end_A = FuzzyDate(reservation.end_dt)
+        
+        start_B = FuzzyDate(self.start_dt)
+        end_B = FuzzyDate(self.end_dt)
+        
+        if start_B <= start_A and end_B >= start_A:
+            self.end_dt = max(self.end_dt, reservation.end_dt)
             return True
-        elif self._is_there_no_gap_between(reservation.end_dt, self.start_dt):
+        elif start_B >= start_A and start_B <= end_A:
+            self.end_dt = max(self.end_dt, reservation.end_dt)
             self.start_dt = reservation.start_dt
             return True
-
+     
         return False
 
     # use some fuzzy logic to figure out if a start_date and end_date are the "same"
     # i.e. only off by two hours or less, or similar.
-    @staticmethod
+    """@staticmethod
     def _is_there_no_gap_between(date1, date2):
         seconds_tolerance = hour_tolerance * 60 * 60
         diff = date1 - date2
         return diff.total_seconds() < seconds_tolerance
+
+    def _fuzzy_equals(date1, date2, seconds_tolerance = hour_tolerance * 60 * 60):
+        diff = date1 - date2
+        return diff.total_seconds() < seconds_tolerance
+            """
--- test_meetingSpaceTimes.py
+++ test_meetingSpaceTimes.py
@@ -6,8 +6,122 @@ __author__ = 'Dom'
 
 
 class TestMeetingSpaceTimes(TestCase):
-    def test_append_new_room_times(self):
+    def test_duplicate_room_times(self):
+        t = MeetingSpaceTimes()
 
+        function_space = "bigass ballroom"
+        start_date1 = t.combine_date_time("1:00 PM", "2/1/2016")
+        end_date1 = t.combine_date_time("4:00 PM", "2/2/2016")
+
+        # do it twice
+        t.append_new_room_times(function_space, start_date1, end_date1)
+        t.append_new_room_times(function_space, start_date1, end_date1)
+
+        self.assertEqual(1, len(t.room_reservations["bigass ballroom"]))
+
+    def assertReservationWasExtended(self, meeting_space_times):
+        self.assertEqual(1, len(meeting_space_times.room_reservations["bigass ballroom"]))
+
+    def assertReservationWasNotExtended(self, meeting_space_times):
+        self.assertEqual(2, len(meeting_space_times.room_reservations["bigass ballroom"]))
+
+    def test_B_no_overlap_A(self):
+        function_space = "bigass ballroom"
+        t = MeetingSpaceTimes()
+        start_dateA = t.combine_date_time("3:00 PM", "2/1/2016")
+        end_dateA = t.combine_date_time("5:00 PM", "2/1/2016")
+        t.append_new_room_times(function_space, start_dateA, end_dateA)
+
+        start_dateB = t.combine_date_time("1:00 PM", "2/1/2016")
+        end_dateB = t.combine_date_time("2:00 PM", "2/1/2016")
+        t.append_new_room_times(function_space, start_dateB, end_dateB)
+
+        self.assertReservationWasNotExtended(t)
+
+    def test_A_no_overlap_B(self):
+        function_space = "bigass ballroom"
+        t = MeetingSpaceTimes()
+        start_dateA = t.combine_date_time("3:00 PM", "2/1/2016")
+        end_dateA = t.combine_date_time("5:00 PM", "2/1/2016")
+        t.append_new_room_times(function_space, start_dateA, end_dateA)
+
+        start_dateB = t.combine_date_time("6:00 PM", "2/1/2016")
+        end_dateB = t.combine_date_time("7:00 PM", "2/1/2016")
+        t.append_new_room_times(function_space, start_dateB, end_dateB)
+        self.assertReservationWasNotExtended(t)
+
+    def test_B_overlap_A(self):
+        function_space = "bigass ballroom"
+        t = MeetingSpaceTimes()
+        start_dateA = t.combine_date_time("3:00 PM", "2/1/2016")
+        end_dateA = t.combine_date_time("5:00 PM", "2/1/2016")
+        t.append_new_room_times(function_space, start_dateA, end_dateA)
+
+        start_dateB = t.combine_date_time("1:00 PM", "2/1/2016")
+        end_dateB = t.combine_date_time("4:00 PM", "2/1/2016")
+        t.append_new_room_times(function_space, start_dateB, end_dateB)
+
+        self.assertReservationWasExtended(t)
+        self.assertEqual(t.room_reservations[function_space][0].start_dt, start_dateB)
+        self.assertEqual(t.room_reservations[function_space][0].end_dt, end_dateA)
+
+    def test_B_encompasses_A(self):
+        function_space = "bigass ballroom"
+        t = MeetingSpaceTimes()
+        start_dateA = t.combine_date_time("3:00 PM", "2/1/2016")
+        end_dateA = t.combine_date_time("5:00 PM", "2/1/2016")
+        t.append_new_room_times(function_space, start_dateA, end_dateA)
+
+        start_dateB = t.combine_date_time("1:00 PM", "2/1/2016")
+        end_dateB = t.combine_date_time("6:00 PM", "2/1/2016")
+        t.append_new_room_times(function_space, start_dateB, end_dateB)
+
+        self.assertReservationWasExtended(t)
+        self.assertEqual(t.room_reservations[function_space][0].start_dt, start_dateB)
+        self.assertEqual(t.room_reservations[function_space][0].end_dt, end_dateB)
+
+    def test_fuzzy_date_le(self):
+        date1 = FuzzyDate(MeetingSpaceTimes.combine_date_time("3:00 PM", "2/1/2016"))
+        date2 = FuzzyDate(MeetingSpaceTimes.combine_date_time("3:10 PM", "2/1/2016"))
+        date3 = FuzzyDate(MeetingSpaceTimes.combine_date_time("4:00 PM", "2/1/2016"))
+
+        self.assertTrue(date1 <= date2)
+        self.assertTrue(date1 == date2)
+
+        self.assertTrue(date1 <= date3)
+        self.assertFalse(date1 == date3)
+
+    def test_B_inside_A(self):
+        function_space = "bigass ballroom"
+        t = MeetingSpaceTimes()
+        start_dateA = t.combine_date_time("3:00 PM", "2/1/2016")
+        end_dateA = t.combine_date_time("5:00 PM", "2/1/2016")
+        t.append_new_room_times(function_space, start_dateA, end_dateA)
+
+        start_dateB = t.combine_date_time("3:30 PM", "2/1/2016")
+        end_dateB = t.combine_date_time("4:30 PM", "2/1/2016")
+        t.append_new_room_times(function_space, start_dateB, end_dateB)
+
+        self.assertReservationWasExtended(t)
+        self.assertEqual(t.room_reservations[function_space][0].start_dt, start_dateA)
+        self.assertEqual(t.room_reservations[function_space][0].end_dt, end_dateA)
+
+    def test_B_overlaps_A_end(self):
+        function_space = "bigass ballroom"
+        t = MeetingSpaceTimes()
+        start_dateA = t.combine_date_time("3:00 PM", "2/1/2016")
+        end_dateA = t.combine_date_time("5:00 PM", "2/1/2016")
+        t.append_new_room_times(function_space, start_dateA, end_dateA)
+
+        start_dateB = t.combine_date_time("4:00 PM", "2/1/2016")
+        end_dateB = t.combine_date_time("6:00 PM", "2/1/2016")
+        t.append_new_room_times(function_space, start_dateB, end_dateB)
+
+        self.assertReservationWasExtended(t)
+        self.assertEqual(t.room_reservations[function_space][0].start_dt, start_dateA)
+        self.assertEqual(t.room_reservations[function_space][0].end_dt, end_dateB)
+
+    def test_append_new_room_times(self):
         t = MeetingSpaceTimes()
 
         function_space = "bigass ballroom"
@@ -19,9 +133,10 @@ class TestMeetingSpaceTimes(TestCase):
         self.assertEqual(1, len(t.room_reservations))
         self.assertEqual(1, len(t.room_reservations["bigass ballroom"]))
 
-        start_date2 = t.combine_date_time("5:00 PM", "2/2/2016")
+        start_date2 = t.combine_date_time("4:20 PM", "2/2/2016")
         end_date2 = t.combine_date_time("4:00 PM", "2/3/2016")
 
+        # test fuzzy date comparison logic. 4:00pm and 4:20pm are close enough that it should see them as the same time
         t.append_new_room_times(function_space, start_date2, end_date2)
 
         self.assertEqual(1, len(t.room_reservations))
@@ -31,7 +146,7 @@ class TestMeetingSpaceTimes(TestCase):
         self.assertEqual(t.room_reservations["bigass ballroom"][0].end_dt, end_date2)
 
         start_date3 = t.combine_date_time("5:00 PM", "1/31/2016")
-        end_date3 = t.combine_date_time("12:00 PM", "2/1/2016")
+        end_date3 = t.combine_date_time("12:45 PM", "2/1/2016")
 
         t.append_new_room_times(function_space, start_date3, end_date3)
 
@@ -64,7 +179,7 @@ class TestMeetingSpaceTimes(TestCase):
             self.assertEqual(t.room_reservations[required_function_space][0].end_dt, end_date1)
 
         function_space2 = "Prince George\'s Exhibit Hall C"
-        start_date2 = t.combine_date_time("1:00 AM", "2/3/2016")
+        start_date2 = t.combine_date_time("12:10 AM", "2/3/2016")
         end_date2 = t.combine_date_time("4:00 PM", "2/3/2016")
 
         t.append_new_room_times(function_space2, start_date2, end_date2)
