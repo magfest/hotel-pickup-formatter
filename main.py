5349701a5a7b05c51d40773f9c671c303fb4118d
Dominic Cerquetti <dom@magfest.org>
2015-07-10 19:20:03 -0400

    open generated CSV files as an html table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from reservation import *
 from meeting_space_times import *
@@ -13,4 +14,9 @@ input_csv = 'markcenter-times.csv'
 # input_csv = "c:/tmp/test-input.csv"
 meeting_space.read_meeting_space_names_from("markcenter-names.txt")
 meeting_space.read_from(input_csv)
-meeting_space.output_csv("c:/tmp/tmpout.csv")
+output_csv = "c:/tmp/tmpout.csv"
+output_html = "c:/tmp/tmpout.html"
+meeting_space.output_csv(output_csv)
+
+os.system('c:\Python34\python.exe csv2html.py ' + output_csv + ' ' + output_html)
+os.system("start " + output_html) # dont put any spaces/weird stuff in filename
